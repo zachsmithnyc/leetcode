0e5d98aabdd2e2f68b4beea8faf6d716cbd7bb72
zachsmithnyc <zachsmithnyc@gmail.com>
2023-08-11 15:31:43 -0400

    practice

--- addtwonumbers.py
+++ addtwonumbers.py
@@ -19,10 +19,16 @@ class LinkedList(object):
     def get_head_node(self):
         return self.head_node
 
-    def insert_beginning(self, new_value):
+    def insert_end(self, new_value):
         new_node = ListNode(new_value)
-        new_node.set_next(self.head_node)
-        self.head_node = new_node
+        current_node = self.get_head_node()
+        new_node.set_next(None)
+        if self.head_node == None:
+            self.head_node = new_node
+            return
+        while current_node.get_next_node() != None:
+            current_node = current_node.get_next_node()
+        current_node.set_next(new_node)
 
     def stringify_list(self):
         stringlist = ""
@@ -44,9 +50,9 @@ class Solution(object):
         list2 = LinkedList()
         list3 = LinkedList()
         for i in l1:
-           list1.insert_beginning(i) 
+           list1.insert_end(i) 
         for i in l2:
-            list2.insert_beginning(i)
+            list2.insert_end(i)
         print(list1.stringify_list())
         print(list2.stringify_list())
 
@@ -55,17 +61,20 @@ class Solution(object):
 
         while node1 and node2 != None:
             x = node1.get_val() + node2.get_val()
+            #print(x)
             if x >= 10:
                 y = x % 10
-                list3.insert_beginning(y)
+                list3.insert_end(y)
+                #print(y)
                 node1 = node1.get_next_node()
                 node2 = node2.get_next_node()
                 x = node1.get_val() + node2.get_val() + 1
-                list3.insert_beginning(x)
+                #print(x)
+                list3.insert_end(x)
                 node1 = node1.get_next_node()
                 node2 = node2.get_next_node()
             else:
-                list3.insert_beginning(x)
+                list3.insert_end(x)
                 node1 = node1.get_next_node()
                 node2 = node2.get_next_node()
 
